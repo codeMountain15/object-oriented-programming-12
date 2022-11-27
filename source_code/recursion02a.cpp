3c06dac265bd66b5e3485a286d9f5c0d1b768b68
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-11-27 14:14:34 +0200

    Update recursion02a.cpp

--- source_code/recursion02a.cpp
+++ source_code/recursion02a.cpp
@@ -24,7 +24,7 @@ void show_successors(FamilyNameLine* ancestor) {
 
     if (!ancestor->successors.empty()) {
         for (auto i = (ancestor->successors).begin(); i != (ancestor->successors).end(); ++i)
-        show_successors(*i);
+            show_successors(*i);
     }
 }
 
@@ -33,20 +33,20 @@ int main() {
     vector<FamilyNameLine*> temp2;
     vector<FamilyNameLine*> temp3;
 
-    FamilyNameLine s4("Takis", "2009");
+    FamilyNameLine s4("Takis", "2020");
     temp1.push_back(&s4);
-    FamilyNameLine s3("Peter", "2011");
+    FamilyNameLine s3("Peter", "2020");
     temp1.push_back(&s3);
 
-    FamilyNameLine s2a("Kate", "1989");
-    FamilyNameLine s2b("John", "1986", temp1);
+    FamilyNameLine s2a("Kate", "2002");
+    FamilyNameLine s2b("John", "1999", temp1);
     temp2.push_back(&s2a);
     temp2.push_back(&s2b);
 
-    FamilyNameLine s1("Sisi", "1988", temp2);
+    FamilyNameLine s1("Sisi", "1980", temp2);
     temp3.push_back(&s1);
 
-    FamilyNameLine s0("Mairy", "1960", temp3);
+    FamilyNameLine s0("Mairy", "1950", temp3);
 
     show_successors(&s0);
 
